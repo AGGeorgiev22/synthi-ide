2315f1c249af13ecee1e7ad7f5caf0e20de9a8c9
aleksandar-synthi <aleksandar@synthi.app>
2026-07-02 18:10:43 +0000

    Mount Lenis smooth scrolling

--- synthi/src/app/page.js
+++ synthi/src/app/page.js
@@ -1,4 +1,5 @@
 import { ScrollProgress } from "@/components/ScrollProgress";
+import { SmoothScroll } from "@/components/SmoothScroll";
 import { Navbar } from "@/components/Navbar";
 import { VectantLanding } from "@/components/VectantLanding";
 import { Footer } from "@/components/Footer";
@@ -6,6 +7,7 @@ import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <>
+      <SmoothScroll />
       <ScrollProgress />
       <Navbar />
       <VectantLanding />
