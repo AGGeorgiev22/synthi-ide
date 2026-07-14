94a1a7a5fa72792a9ba5df404df436d92e69f177
aleksandar-synthi <aleksandar@synthi.app>
2026-07-14 20:18:49 +0000

    feat(runtime): unify guarded run into one scroll spine

--- synthi/src/components/VectantLanding.jsx
+++ synthi/src/components/VectantLanding.jsx
@@ -1,13 +1,11 @@
-import { AuthorityStory } from "@/components/home/AuthorityStory";
 import { CinematicHero } from "@/components/home/CinematicHero";
 import { ControlPlaneAtlas } from "@/components/home/ControlPlaneAtlas";
 import { EvidenceStrip } from "@/components/home/EvidenceStrip";
 import { FieldNotes } from "@/components/home/FieldNotes";
 import { FinalApproach } from "@/components/home/FinalApproach";
 import { GpuProofChapter } from "@/components/home/GpuProofChapter";
+import { GuardedRunCinema } from "@/components/home/GuardedRunCinema";
 import { MissionProfiles } from "@/components/home/MissionProfiles";
-import { RunBoundary } from "@/components/home/RunBoundary";
-import { RuntimeFeedback } from "@/components/home/RuntimeFeedback";
 import styles from "@/components/VectantLanding.module.css";
 
 export function VectantLanding() {
@@ -15,10 +13,8 @@ export function VectantLanding() {
     <main id="main-content" tabIndex={-1} className={styles.page}>
       <CinematicHero />
       <EvidenceStrip />
-      <RunBoundary />
-      <AuthorityStory />
+      <GuardedRunCinema />
       <GpuProofChapter />
-      <RuntimeFeedback />
       <ControlPlaneAtlas />
       <MissionProfiles />
       <FieldNotes />
