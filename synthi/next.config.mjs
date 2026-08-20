681440ca7f21d925cb69bf395113d47a827de874
AMKolev22 <amkolev22@codingburgas.bg>
2026-08-21 02:44:20 +0300

    test

--- synthi/next.config.mjs
+++ synthi/next.config.mjs
@@ -7,7 +7,6 @@ const appDir = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 5120, 5760],
-    qualities: [75, 95],
   },
   turbopack: {
     root: appDir,
